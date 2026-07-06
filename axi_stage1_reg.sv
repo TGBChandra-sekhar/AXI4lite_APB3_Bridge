22c6963a74f4368a0e0c127cfdff02dd61f54a81
Tanuku Gnanendra Bala Chandra Sekhar <chandrasekhar.tgb@gmail.com>
2026-07-06 17:28:03 +0530

    Update axi_stage1_reg.sv

--- axi_stage1_reg.sv
+++ axi_stage1_reg.sv
@@ -1,1 +1,25 @@
-
+module axi_stage1_reg (
+    input  wire        aclk,
+    input  wire        aresetn,
+    input  wire        in_valid,
+    input  wire        in_write,
+    input  wire [31:0] in_addr,
+    input  wire [31:0] in_wdata,
+    output reg         out_valid,
+    output reg         out_write,
+    output reg [31:0]  out_addr,
+    output reg [31:0]  out_wdata
+);
+    always @(posedge aclk or negedge aresetn) begin
+        if (!aresetn)
+            out_valid <= 0;
+        else begin
+            out_valid <= in_valid;
+            if (in_valid) begin
+                out_write <= in_write;
+                out_addr  <= in_addr;
+                out_wdata <= in_wdata;
+            end
+        end
+    end
+endmodule
